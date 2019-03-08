438818e95aa672d27a9db72ac11185ad66d2b906
ben b <beunra@yahoo.com>
2019-03-08 22:33:08 +0300

    Add files via upload

---
+++ hashcode2019/factorial.js
@@ -0,0 +1,17 @@
+function factor(){
+
+let mem = {}
+ return function factorialize(n) {
+  if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+  else {
+      if (n in mem) { return mem[n] )
+      memo[n] = (num * factorialize(num - 1));
+      return mem[n];
+  }
+}
+}
+}
+
